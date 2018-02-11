3ba849f7692f037346e5cc0f7c5ff79377e6a13e
Thiago Mendes <thiago@posteo.de>
2018-02-11 00:03:07 -0200

    single_list: cmocha test
    add another test to the single list

--- 001.lists/singlelinked/C/cmocka_test.c
+++ 001.lists/singlelinked/C/cmocka_test.c
@@ -11,6 +11,8 @@
 #include "main.h"
 #include "list.h"
 
+#define KEY_TEST_A 10
+
 static int setup(void **state) {
     list_t * list = list_init(compare, print);
     *state = list;
@@ -34,15 +36,29 @@ static void list_test_insert_a_node(void **state) {
     int8_t ret;
     data_t * node = malloc(sizeof(*node));
     assert_non_null(node);
-    node->key = 0;
+    node->key = KEY_TEST_A;
     ret = list_ins_in_order(list, node);
     assert_int_equal(ret, LST_SUCCESS);
+}
+
+static void list_test_lookup_for_a_node(void **state) {
+    list_test_create_a_list(state);
+    list_test_insert_a_node(state);
+    list_t *list = *state;
+    data_t * node_to_find = malloc(sizeof(*node_to_find));
+    assert_non_null(node_to_find);
+    node_to_find->key = KEY_TEST_A;
+    data_t * node = list_lookup(list, node_to_find);
+    assert_non_null(node);
+    assert_int_equal(node->key, KEY_TEST_A);
+    free(node_to_find);
 }
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test_setup_teardown(list_test_create_a_list, setup, teardown),
         cmocka_unit_test_setup_teardown(list_test_insert_a_node, setup, teardown),
+        cmocka_unit_test_setup_teardown(list_test_lookup_for_a_node, setup, teardown),
     };
     return cmocka_run_group_tests_name("success_test", tests, NULL, NULL);
 }
