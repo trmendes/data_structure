cd8a4ab8427bb93781872ef1f1579504fa251733
Thiago Mendes <thiago@posteo.de>
2018-04-02 19:16:17 -0300

    javascript: bst draft

--- 004.bst/bst/Javascript/bst.js
+++ 004.bst/bst/Javascript/bst.js
@@ -11,15 +11,50 @@ const BST = function() {
         this.parent = null;
     };
 
-    this.insert = function(data) {};
+    this.insert = function(data) {
+        if (data === undefined) {
+            return;
+        }
+
+        if (isNaN(data)) {
+            return;
+        }
+
+        let node = new Node(data);
+
+        let pNode = this._root;
+
+        while (pNode !== null) {
+            if (pNode.data >= data) {
+                pNode = pNode.left;
+            } else {
+                pNode = pNode.right;
+            }
+        }
+
+        pNode = node;
+
+    };
 
     this.remove = function(data) {};
 
     this.find = function(data) {};
 
-    this.max = function() {};
+    this.max = function() {
+        let pNode = this._root;
+        while (pNode.right) {
+            pNode = pNode.right;
+        }
+        return pNode.data;
+    };
 
-    this.min = function() {};
+    this.min = function() {
+        let pNode = this._root;
+        while (pNode.left) {
+            pNode = pNode.left;
+        }
+        return pNode.data;
+    };
 
     this.pre = function(data) {};
 
